f7e25d0c8150297ccd5760e95ff1e39bd027a481
Schwarz <kschwa@f0189856d7ec.ant.amazon.com>
2020-05-06 10:56:39 -0400

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-kevin-schwarz"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://mng09373o0.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_zrIEqLr4Q",
+    APP_CLIENT_ID: "3k2c5o89c7cc1a164trejdfieu",
+    IDENTITY_POOL_ID: "us-east-2:4a8c2278-42af-4dc5-93e5-4915b3dcb508"
+  }
+};
